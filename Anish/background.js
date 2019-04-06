afd5eb8864fc3a6690005a4cad11a7965b1aea63
PashaHakim <36797045+PashaHakim@users.noreply.github.com>
2019-04-06 15:19:58 -0400

    Static box

---
+++ Anish/background.js
@@ -0,0 +1,31 @@
+// this is the background code...
+//print("Code loaded")
+// alert("I havxx`x`e loaded")
+
+chrome.browserAction.onClicked.addListener(function (currentTab) {
+	// for the current tab, inject the "inject.js" file & execute it
+
+	var url;
+
+	// alert("something happened")
+
+	chrome.tabs.getSelected(null, function(tab) {
+		handleStuff(tab.url, currentTab);
+	})
+
+
+
+});
+
+function handleStuff(tablink, currentTab){
+
+	if(tablink.indexOf("chrome://") != -1){
+		alert("Chrome link detected");
+		return;
+	}
+
+	chrome.tabs.executeScript(currentTab.ib, {
+		file: 'inject.js'
+	});
+
+}
---
+++ Anish/inject.js
@@ -0,0 +1,32 @@
+// this is the code which will be injected into a given page...
+
+(function() {
+
+	var ele = document.getElementById("anishimage");
+	if(ele != null){
+		ele.remove();
+		return;
+	}
+
+	//This inserts an html file
+	var iframe  = document.createElement ('iframe');
+	iframe.classList.add('relative');
+	relative = document.createElement('style');
+	relative.innerHTML = '.relative { position: static }';
+	document.body.appendChild(relative);
+	iframe.src  = chrome.extension.getURL ('template.html');
+	document.body.appendChild(iframe);
+  // document.body.insertBefore(iframe, document.body.getElementsByTagName('p')[3]);  // Insert <li> before the first child of <ul>
+
+	// fetch(chrome.extension.getURL('template.html'))
+  //   .then(response => response.text())
+  //   .then(data => {
+  // 		document.body.innerHTML = data + document.body.innerHTML;
+  //       // other code
+  //       // eg update injected elements,
+  //       // add event listeners or logic to connect to other parts of the app
+  //   }).catch(err => {
+  //       // handle error
+  //   });
+
+})();
