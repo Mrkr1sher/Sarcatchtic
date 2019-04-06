6901d588c7339dd6c603e277d851137311ca8a99
PashaHakim <36797045+PashaHakim@users.noreply.github.com>
2019-04-06 12:55:30 -0400

    s

--- Extension/Anish/inject.js
+++ Extension/Anish/inject.js
@@ -11,7 +11,7 @@
 	//This inserts an html file
 	var iframe  = document.createElement ('iframe');
 	iframe.src  = chrome.extension.getURL ('template.html');   // Get the <ul> element to insert a new node
-document.body.insertBefore(iframe, document.body.getElementByTagName('p')[0]);  // Insert <li> before the first child of <ul>
+document.body.insertBefore(iframe, document.body.getElementsByTagName('p')[0]);  // Insert <li> before the first child of <ul>
 
 	// fetch(chrome.extension.getURL('template.html'))
   //   .then(response => response.text())
