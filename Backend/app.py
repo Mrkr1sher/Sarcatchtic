0084265b00470ab2896c58305099c61c9ed13863
Timothy Miles <tim@ns519440.ip-158-69-23.net>
2019-04-06 15:49:01 +0000

    added flask

--- Backend/app.py
+++ Backend/app.py
@@ -1,10 +1,15 @@
-from Flask import *
+from flask import *
 import processor
 
-app = Flask(__main__)
+app = Flask(__name__)
+
+@app.route("/")
+def handle():
+    return "Nothing"
 
 @app.route("/submit")
 def handleInput():
-    return proccessor.process(request.form['input'])
+    print("Received request" + request.form['input'])
+    return "Nothing" #proccessor.process(request.form['input'])
     
-
+app.run(ssl_context='adhoc',host='0.0.0.0',port=80)
