814ef638a4df05d2c494179770409829c0aa4e7b
PashaHakim <36797045+PashaHakim@users.noreply.github.com>
2019-04-06 10:07:42 -0400

    Inserted code

--- Extension/Anish/inject.js
+++ Extension/Anish/inject.js
@@ -8,15 +8,26 @@
 		return;
 	}
 
+	//This inserts an html file
+	var xmlHttp = null;
+
+	xmlHttp = new XMLHttpRequest();
+	xmlHttp.open( "GET", chrome.extension.getURL ("src/inject/inject.html"), false );
+	xmlHttp.send( null );
+
+	var inject  = document.createElement("div");
+	inject.innerHTML = xmlHttp.responseText
+	document.body.insertBefore (inject, document.body.firstChild);
+
 	// just place a div at top right
 	var div = document.createElement('p');
 	div.style.position = 'fixed';
 	div.style.bottom = 0;
 	div.style.left = 0;
 	div.id = "anishimage"
-	
+
 	div.textContent = "Whatever can be inserted in"
-	
+
 	document.body.appendChild(div);
 	div.style.zIndex = 100;
 
